14cbf191b6b28716b3a904050cbb21498e03d9cb
taohui <htao@thoughtworks.com>
2022-08-13 11:10:51 +0800

    adjust expense date css style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import Expenses from "./components/Expenses";
 
 function App() {
-  const expenseItemArray = [{
-    id: "1",
-    title: "House Issurance",
-    date: "2011-05-01",
-    amount: 700
-}, {
-    id: "2",
-    title: "New Car",
-    date: "2012-01-01",
-    amount: 3000
-}, {
-    id: "3",
-    title: "New TV",
-    date: "2013-07-06",
-    amount: 350
-}]
+  const expenseItemArray = [
+    {
+      id: "1",
+      title: "House Issurance",
+      date: "2011-12-01",
+      amount: 700,
+    },
+    {
+      id: "2",
+      title: "New Car",
+      date: "2012-01-01",
+      amount: 3000,
+    },
+    {
+      id: "3",
+      title: "New TV",
+      date: "2013-11-06",
+      amount: 350,
+    },
+  ];
 
-  return (
-    <Expenses items={expenseItemArray} />
-  );
+  return <Expenses items={expenseItemArray} />;
 }
 
 export default App;
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
   const date = new Date(props.date);
   const year = date.getFullYear();
   const month = date.toLocaleString("default", { month: "long" });
   const day = date.toLocaleString("default", { day: "2-digit" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
