303c737db5020d97fce776e33bac328ca1fb10d7
taohui <htao@thoughtworks.com>
2022-08-19 09:41:46 +0800

    fix test warnings

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -2,7 +2,7 @@ import "./ExpenseDate.css";
 
 function ExpenseDate(props) {
   const date = new Date(props.date);
-  const year = date.getFullYear();
+  const year = date.getFullYear().toString();
   const month = date.toLocaleString("default", { month: "long" });
   const day = date.toLocaleString("default", { day: "2-digit" });
   return (
