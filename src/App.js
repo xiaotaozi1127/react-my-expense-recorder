df6025d1082b6f0be8e91f3247f3e0f11fe6bb25
taohui <htao@thoughtworks.com>
2022-09-05 09:11:50 +0800

    fetch server data for the init expenses

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import MyContext from "./components/Contexts/MyContext";
 
 function App() {
-  const expenseItemArray = [
-    {
-      id: "1",
-      title: "House Issurance",
-      date: "2019-12-01",
-      amount: 700,
-    },
-    {
-      id: "2",
-      title: "New Car",
-      date: "2020-01-01",
-      amount: 3000,
-    },
-    {
-      id: "3",
-      title: "New TV",
-      date: "2021-11-06",
-      amount: 350,
-    },
-  ];
-  const [expenses, setExpenses] = useState(expenseItemArray);
+  const [expenses, setExpenses] = useState([]);
+
+  const fetchExpenseItems = useCallback(async () => {
+    const url = "http://localhost:8080";
+    const response = await fetch(url);
+    const data = await response.json();
+    setExpenses(data);
+  }, []);
+
+  useEffect(() => {
+    fetchExpenseItems();
+  }, [fetchExpenseItems]);
+
   const newExpenseCreateHandler = (newExpense) => {
     setExpenses((prevState) => {
       return [newExpense, ...prevState];
